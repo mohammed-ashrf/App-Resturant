795f6ebaba082fa17861d3f4c27032dcd3dc8b40
mohammed-ashrf <greenbile2002@gmail.com>
2021-07-19 02:02:17 +0200

    promises part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -63,7 +64,8 @@ import { LoginComponent } from './login/login.component';
     MatInputModule,
     MatCheckboxModule,
     MatSelectModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     DishService,
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -9,14 +9,23 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second deley
+      setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
   getleader(id: string):  Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id ===id)) [0]);
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second deley
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id ===id)) [0]), 2000)
+    });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second deley
+      setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000)
+    });
   }  
 }
